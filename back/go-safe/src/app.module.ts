3d7706b42a8c12d7996524160eaa466c242e31cd
Sebastian Aguilar <joansaguilarj@gmail.com>
2026-03-05 17:09:05 -0500

    Feat: se agrega conexion con BD typeORM

--- back/go-safe/src/app.module.ts
+++ back/go-safe/src/app.module.ts
@@ -6,9 +6,27 @@ import { GuidesModule } from './guides/guides.module';
 import { ExperiencesModule } from './experiences/experiences.module';
 import { OrdersModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeorm from './config/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, GuidesModule, ExperiencesModule, OrdersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeorm')!,
+    }),
+    UsersModule,
+    GuidesModule,
+    ExperiencesModule,
+    OrdersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

7ab2289fd0f3032fe6642852ed7813532391aec9
AngelaMiniello <angela.min03@gmail.com>
2026-03-07 19:34:07 -0300

    fix: restauracion de page.tsx y not-found

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex h-screen justify-between items-center flex-col`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex h-screen items-center flex-col`}
       >
+      <Navbar/>
+      {children}
       </body>
     </html>
   );
---
+++ front/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Página no encontrada</h2>
+      <p>Lo sentimos, no pudimos encontrar lo que buscabas.</p>
+    </div>
+  );
+}

4cbe86691568abd884a4d3e0023bae590a863ffc
JorgeHernandez-code <jorhu-95hotmail.com>
2026-03-08 17:33:39 -0500

    mis cambios locales

--- back/go-safe/src/orders/entities/order.entity.ts
+++ back/go-safe/src/orders/entities/order.entity.ts
@@ -1,1 +1,15 @@
-export class Order {}
+import { Exclude } from "class-transformer";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({
+  name: 'orders',
+})
+export class Order {
+  @Exclude()
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({
+    type: 
+  })
+}

a1ce9ee2bed7da44399d7e926a02703315af150a
Sebastian Aguilar <joansaguilarj@gmail.com>
2026-03-09 09:43:35 -0500

    feat: crud experiences entity

--- back/go-safe/src/experiences/experiences.service.ts
+++ back/go-safe/src/experiences/experiences.service.ts
@@ -1,26 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateExperienceDto } from './dto/create-experience.dto';
 import { UpdateExperienceDto } from './dto/update-experience.dto';
+import { Experience } from './entities/experience.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ExperiencesService {
-  create(createExperienceDto: CreateExperienceDto) {
-    return 'This action adds a new experience';
+  constructor(
+    @InjectRepository(Experience)
+    private experiencesRepository: Repository<Experience>,
+  ) {}
+
+  async create(createExperienceDto: CreateExperienceDto) {
+    try {
+      const newExperience: Experience =
+        this.experiencesRepository.create(createExperienceDto);
+      await this.experiencesRepository.save(newExperience);
+      return newExperience;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all experiences`;
+  async findAll() {
+    const experiencesFound = await this.experiencesRepository.find();
+    return experiencesFound;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} experience`;
+  async findOne(id: string) {
+    try {
+      const experiencesFound = await this.experiencesRepository.findOneBy({
+        id,
+      });
+
+      if (!experiencesFound)
+        throw new NotFoundException('Experience not found');
+
+      return experiencesFound;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
-  update(id: number, updateExperienceDto: UpdateExperienceDto) {
-    return `This action updates a #${id} experience`;
+  async update(id: string, updateExperienceDto: UpdateExperienceDto) {
+    const experienceFound = await this.experiencesRepository.findOneBy({
+      id,
+    });
+    if (!experienceFound) throw new NotFoundException('Experience not found');
+
+    try {
+      const newExperience = updateExperienceDto;
+      await this.experiencesRepository.update(id, newExperience);
+      return newExperience;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} experience`;
+  async remove(id: string) {
+    const experienceFound = await this.experiencesRepository.findOneBy({ id });
+    if (!experienceFound) throw new NotFoundException('Experience not found');
+    await this.experiencesRepository.delete(id);
+
+    return { deleted: true };
   }
 }

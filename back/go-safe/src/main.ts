3b0f039f2c216ef1c04e63d9f4028b17941b0de8
Sebastian Aguilar <joansaguilarj@gmail.com>
2026-03-10 13:37:03 -0500

    feat: se implementa swagger

--- back/go-safe/src/main.ts
+++ back/go-safe/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Go-Safe API Documentation')
+    .setDescription('documentation of Go-safe project')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

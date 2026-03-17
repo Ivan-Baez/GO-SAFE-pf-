12c9c92a95c62c7f1892dd9e713b08923b613328
AngelaMiniello <angela.min03@gmail.com>
2026-03-17 13:22:50 -0300

    nav estilo

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -3,7 +3,7 @@
 import { useAuth } from "@/context/AuthContext";
 import Image from "next/image";
 import Link from "next/link";
-
+import { UserRound, ShoppingCart, LogOutIcon } from 'lucide-react';
 
 export default function Navbar() {
 
@@ -21,63 +21,67 @@ export default function Navbar() {
           height={80}
         />
       </Link>
-      <div>
-        {userData?.token ? (
-          <>
-        <div>
-          {/* 1. Barra de búsqueda  */}
-        <div className="flex-1 flex justify-center">
-          <input
-            type="text"
-            placeholder="Busca por aventura"
-            className="w-full pl-12 pr-4 py-2.5 rounded-2xl bg-gray-50 border border-gray-200 
-                     focus:bg-white focus:ring-2 focus:ring-[#EAB308]/20 focus:border-[#EAB308] 
-                     outline-none transition-all duration-300 text-sm shadow-sm"
-          />
-        </div>
-        {/* 2. Acciones del Usuario */}
-      </div>
-            <Link 
-              href="/dashboard" 
-              className="flex items-center gap-2 text-sm font-semibold text-gray-600 hover:text-gray-900 
-                   bg-white px-4 py-2 rounded-xl border border-gray-100 hover:shadow-sm transition-all active:scale-95"
-            >
-            <span className="text-base">👤</span> Dashboard
-            </Link>
 
-            <Link 
-              href="/cart" 
-              className="flex items-center gap-2 text-sm font-semibold text-gray-600 hover:text-gray-900 
-                   relative bg-white px-4 py-2 rounded-xl border border-gray-100 hover:shadow-sm transition-all active:scale-95"
-            >
-              <span className="text-base">🛒</span> Carrito
-            </Link>
-            <button
-              onClick={handleLogout}
-              className="flex items-center gap-2 text-sm font-bold text-red-500 hover:text-red-600 
-                   hover:bg-red-50 px-4 py-2 rounded-xl transition-all active:scale-95"
-            >
-             <span>🚪</span> Salir
-            </button>
-          </>
-        ) : (
-          <div className="flex gap-4">
-            <Link
-              href="/login"
-              className="px-4 py-2 border border-black rounded hover:bg-[#b8b1a6] w-37.5"
-            >
-              Ingresar
-            </Link>
-
-            <Link
-              href="/RegisterForBoth"
-              className="px-4 py-2 border border-black rounded hover:bg-[#b8b1a6] w-37.5"
-            >
-              Registrarme
-            </Link>
-          </div>
-        )}
+      {/* CONTENIDO */}
+      {userData?.token ? (
+      <div className="flex items-center gap-6 w-full justify-end">
+      
+      {/* BUSCADOR */}
+      <div className="flex-1 flex justify-center">
+        <input
+          type="text"
+          placeholder="Busca por aventura"
+          className="w-full max-w-md px-4 py-2.5 rounded-2xl bg-gray-50 border border-gray-200 
+          focus:bg-white focus:ring-2 focus:ring-[#EAB308]/20 focus:border-[#EAB308] 
+          outline-none transition-all duration-300 text-sm shadow-sm"
+        />
       </div>
+
+      {/* ACCIONES */}
+      <div className="flex items-center gap-4">
+        
+        <Link 
+          href="/dashboard" 
+          className="flex items-center gap-2 px-4 py-2 border border-black rounded hover:bg-[#b8b1a6] justify-center text-center"
+        >
+          <UserRound size={18} />
+          <span>Dashboard</span>
+        </Link>
+
+        <Link 
+          href="/cart" 
+          className="flex items-center gap-2 px-4 py-2 border border-black rounded hover:bg-[#b8b1a6] justify-center text-center"
+        >
+          <ShoppingCart size={18} />
+          <span>Carrito</span>
+        </Link>
+
+        <button
+          onClick={handleLogout}
+          className="flex items-center gap-2 px-4 py-2 border border-black rounded hover:bg-[#b8b1a6] justify-center text-center"
+        >
+          <LogOutIcon size={18} />
+          <span>Salir</span>
+        </button>
+      </div>
+      </div>
+
+      ) : (
+      <div className="flex gap-4">
+      <Link
+        href="/login"
+        className="px-4 py-2 border border-black rounded hover:bg-[#b8b1a6] w-37.5 flex items-center justify-center gap-2 text-center"
+      >
+        Ingresar
+      </Link>
+
+      <Link
+        href="/RegisterForBoth"
+        className="px-4 py-2 border border-black rounded hover:bg-[#b8b1a6] w-37.5 flex items-center justify-center gap-2 text-center"
+      >
+        Registrarme
+      </Link>
+      </div>
+      )}
     </nav>
-  );
-}
+  )}

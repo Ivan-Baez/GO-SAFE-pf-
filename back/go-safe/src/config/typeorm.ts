69111b9121ca091c56ddd6d2c783faabbd981718
Sebastian Aguilar <joansaguilarj@gmail.com>
2026-03-17 19:16:20 -0500

    feat: ajuste typeorm deploy

--- back/go-safe/src/config/typeorm.ts
+++ back/go-safe/src/config/typeorm.ts
@@ -7,16 +7,12 @@ dotenvConfig({ path: '.development.env' });
 
 export const config = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: ['dist/**/*.entity{.ts,.js}'],
-  dropSchema: false,
+  url: process.env.DATABASE_URL,
+  autoLoadEntities: true,
   synchronize: true,
-  logging: true,
-  migrations: [],
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 export default registerAs('typeorm', () => config);

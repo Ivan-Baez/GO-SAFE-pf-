c414602dd9ea4278e44c27fa2e8364dbab067b7c
Sebastian Aguilar <joansaguilarj@gmail.com>
2026-03-20 13:22:48 -0500

    feat: se ajusta creacion de instructor con entidades 2

---
+++ back/go-safe/src/certifications/entities/certification.entity.ts
@@ -0,0 +1,24 @@
+import { Instructor } from 'src/instructors/entities/instructor.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('certifications')
+export class Certifications {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: false })
+  title!: string;
+
+  @Column({ type: 'text', nullable: false })
+  url!: string;
+
+  @ManyToOne(() => Instructor, (instructor) => instructor.certifications)
+  @JoinColumn({ name: 'instructorId' })
+  instructor!: Instructor;
+}
